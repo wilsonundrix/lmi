309dceefaaae7006d6157fe5d3eb968b8cb84bea
ndunyaleaf <ndunyaleaf@gmail.com>
2021-03-21 14:12:58 +0300

    Initial Commit with working laravel tailwindcss and vue.js integration.

---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="bg-blue-600">
+        Hello Friends
+    </div>
+
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('hello Component mounted.')
+    }
+}
+</script>
